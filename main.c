9124dce369aae29cd0f27edf6b94ae95b054faf5
LittleWatson21 <esalazar5@ucol.mx>
2019-06-06 17:29:39 -0500

    Primero paso

--- main.c
+++ main.c
@@ -2,9 +2,12 @@
 #use delay(clock=16M)
 void main()
 {
- int8 lectura;
+set_tris_a(0xFF)
+set_tris_b(0xFF)
+ int16 lectura;
    while(TRUE)
    {
+   
    setup_adc_ports(AN0);
    setup_adc(ADC_CLOCK_INTERNAL);
    set_adc_channel(0);
